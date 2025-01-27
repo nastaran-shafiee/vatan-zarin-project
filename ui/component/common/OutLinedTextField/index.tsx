bd18b25978ec5f2e5a086f8b82ec265f3ae73ce6
Nastaran Shafiee <115412256+nastaran-shafiee@users.noreply.github.com>
2025-01-27 12:19:31 +0330

    custom textfiled

--- ui/component/common/OutLinedTextField/index.tsx
+++ ui/component/common/OutLinedTextField/index.tsx
@@ -1,8 +1,15 @@
 import { TextField, styled, TextFieldProps } from '@mui/material';
 
 export const OutLinedTextField = styled(({ ...props }: TextFieldProps) => (
-  <TextField fullWidth size="small" {...props} />
+  <TextField fullWidth size="small" {...props}  />
 ))(({ theme }) => ({
+  '& .MuiOutlinedInput-root': {
+    height: '50px', 
+  },
+  '& .MuiInputBase-input': {
+    padding: '12px 14px',
+    height: 'auto', 
+  },
   '& .MuiFormLabel-asterisk': {
     color: theme.palette.error.main,
   },
