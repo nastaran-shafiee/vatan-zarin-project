4486d92a239c521a13153bd836f4d3d9c0a21194
Nastaran Shafiee <115412256+nastaran-shafiee@users.noreply.github.com>
2025-01-27 12:23:09 +0330

    add all page add desighn

--- app/[lang]/(main)/courses/[courseManagement]/page.tsx
+++ app/[lang]/(main)/courses/[courseManagement]/page.tsx
@@ -1,32 +1,46 @@
 "use client";
 import Header from "#/ui/component/common/Header";
-import { Box, Container, Grid, LinearProgress } from "@mui/material";
-import { useTranslations } from "next-intl";
-import { useParams, useRouter } from "next/navigation";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import FileUpload from "#/ui/component/common/FileUpload";
-const CourseManagement = () => {
-  const params = useParams();
-  const courseManagement = params?.courseManagement;
-  const router = useRouter();
-  const t = useTranslations();
-
-"use client";
-import Header from "#/ui/component/common/Header";
-import { Box, Container, Grid, LinearProgress } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  LinearProgress,
+  Typography,
+} from "@mui/material";
 import { useTranslations } from "next-intl";
 import { useParams, useRouter } from "next/navigation";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import TextFiledFileUpload from "#/ui/component/common/TextFiledFileUpload";
+import FormInputText from "#/ui/component/common/FormTextFiled";
+import { FormProvider, useForm } from "react-hook-form";
+import { useMemo } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { OutLinedTextField } from "#/ui/component/common/OutLinedTextField";
+import FormSelect from "#/ui/component/common/FormSelect";
+
 const CourseManagement = () => {
   const params = useParams();
   const courseManagement = params?.courseManagement;
   const router = useRouter();
   const t = useTranslations();
 
+  const schema = useMemo(
+    () =>
+      yup.object().shape({
+        username: yup.string().required(t("F_PorKardanElzami")),
+        password: yup.string().required(t("F_PorKardanElzami")),
+      }),
+    [t]
+  );
+  const methods = useForm({
+    resolver: yupResolver(schema),
+    reValidateMode: "onSubmit",
+  });
 
   return (
-    <>
+    <Container maxWidth="md" sx={{ px: 0 }}>
       <Header
         text={t("List_courses")}
         isTheme={false}
@@ -39,22 +53,60 @@ const CourseManagement = () => {
         } // Custom back icon
       />
 
-      <Container fixed sx={{ "&.MuiContainer-root": { px: 0 } }}>
+      <Box
+        sx={{
+          marginTop: "16px",
+          marginBottom: "20px",
+          bgcolor: "background.paper",
+          paddingX: "8px",
+          paddingY: "24px",
+        }}
+      >
+        <TextFiledFileUpload
+          title="کاور"
+          isRequired={true}
+          label="افزودن کاور"
+        />
+      </Box>
+      <FormProvider {...methods}>
         <Box
           sx={{
-            width: "100%",
-            marginTop:"16px",
+            marginTop: "16px",
             marginBottom: "20px",
             bgcolor: "background.paper",
-            paddingX:"8px", 
-            paddingY:"24px"
+            paddingX: "8px",
+            paddingY: "24px",
           }}
         >
-         
-          <TextFiledFileUpload  title="کاور" isRequired={true}  label="افزودن کاور" />
+          <Typography variant="h6">اطلاعات دوره</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "18px",
+              marginY: "24px",
+            }}
+          >
+            <OutLinedTextField label={"نام دوره"} name={"name"} required />
+            <OutLinedTextField label={" توضیحات"} name={"name"} required />
+            <FormSelect name="" label="زبان " required />
+            <FormSelect name="" label="حداقل رنک " required />
+          </Box>
         </Box>
-      </Container>
-    </>
+        <Box
+          sx={{
+            marginTop: "16px",
+            bgcolor: "background.paper",
+            paddingX: "8px",
+            paddingY: "24px",
+          }}
+        >
+          <Button variant="contained" color="primary" fullWidth sx={{paddingY:"7.5px"}}>
+            ارسال دوره
+          </Button>
+        </Box>
+      </FormProvider>
+    </Container>
   );
 };
 
